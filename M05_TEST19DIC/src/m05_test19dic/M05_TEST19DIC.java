93b993031f2fb71bc7c87f98bdbb6f2d71e431d8
ausias <ausias@hINFO6-12.xiam.cat>
2022-12-19 11:42:38 +0100

    Primer commit

---
+++ M05_TEST19DIC/src/m05_test19dic/M05_TEST19DIC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package m05_test19dic;
+
+/**
+ *
+ * @author ausias
+ */
+public class M05_TEST19DIC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
